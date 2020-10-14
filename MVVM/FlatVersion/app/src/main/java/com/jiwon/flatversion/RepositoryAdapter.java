27d948b12f4cd4df8f508a7ea865ba18d00850a4
jiwon <wldnjs594@naver.com>
2020-10-14 09:34:24 +0900

    GitHubService 인터페이스 생성
    Retrofit 이용

---
+++ MVVM/FlatVersion/app/src/main/java/com/jiwon/flatversion/RepositoryAdapter.java
@@ -0,0 +1,12 @@
+package com.jiwon.flatversion;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RepositoryAdapter  {
+
+
+    // 레포지토리의 아이템이 탭되면 호출됨
+    interface OnRepositoryItemClickListener{
+        void onRepositoryItemClick();
+    }
+}
---
+++ MVVM/FlatVersion/app/src/main/java/com/jiwon/flatversion/RepositoryListAcitivity.java
@@ -0,0 +1,6 @@
+package com.jiwon.flatversion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RepositoryListAcitivity extends AppCompatActivity {
+}
